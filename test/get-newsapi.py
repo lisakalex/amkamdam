92055bdf301be3b5496b20eb96b0941d060bbcdb
al <lisakalex@gmail.com>
2024-08-30 05:50:30 +0100

    load newsapi and add to boxing-1.json

---
+++ test/get-newsapi.py
@@ -0,0 +1,33 @@
+#!/home/al/.venv/bin/python3
+from bs4 import BeautifulSoup
+import requests
+import time
+from pathlib import Path
+from datetime import datetime, timedelta, date
+import os
+import json
+
+
+# https://beautiful-soup-4.readthedocs.io/en/latest/
+
+
+def paged(keyword):
+    y = date.today() - timedelta(days=1)
+    yesterday = y.strftime('%Y-%m-%d')
+    url = ('https://newsapi.org/v2/everything?'
+           'q=' + keyword +
+           '&' + yesterday +
+           '&sortBy=publishedAt'
+           '&language=en'
+           '&apiKey=41e2e097fbb4457c9b714ee6acd4185b')
+
+    with open('newsapi-' + str(yesterday) + '.json', 'w') as f:
+        f.write(requests.get(url).text)
+
+
+def main():
+    paged('sport')
+
+
+if __name__ == '__main__':
+    main()
